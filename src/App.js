5ac1dfa7e77d165bbb442a4c38dd62caa4039c96
Prasham1710 <jainprasham17@gmail.com>
2023-01-14 19:54:04 +0530

    final subsmission

--- src/App.js
+++ src/App.js
@@ -1,44 +1,83 @@
-    import React from 'react'
-    import { useState } from "react";
+import { useState } from "react";
 
-    const App = () => {
-       const [weight, setWeight] = useState(0)
-  const [height, setHeight] = useState(0)
-  const [bmi, setBmi] = useState('')
-  const [message, setMessage] = useState('')
-      if (weight === 0 || height === 0) {
-      alert('Please enter a valid weight and height')
-    } else {
-      let bmi = (weight / (height * height) * 703)
-      if (bmi < 25) {
-        setMessage('You are underweight')
-      } else if (bmi >= 25 && bmi < 30) {
-        setMessage('You are a healthy weight')
-      } else {
-        setMessage('You are overweight')
-      }
-      return (
-        <div className="w-full max-w-xs m-10">
-          <div className='container'>
-            <h2 className='text-center mb-4 text-xl'>BMI Calculator</h2>
-            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-              <div className="mb-4"><label
-            className="block text-gray-700 text-sm font-bold mb-2">Weight (lbs)</label>
-              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="weight" type ="Weight in kg " placeholder='weight in kg' ></input></div>
-              <div>
-                <label className="block text-gray-700 text-sm font-bold mb-2">Height (in)</label>
-                <input
-                className="shadow appearance-none  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                id="Height "
-                type="text"
-                placeholder="Height in cm"></input></div>
-                <div>
-            <button className="bg-purple-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
- type='submit'>Submit</button><div className="mt-4">
-            <p>Your BMI is:  </p>
-            
-          </div></div></form></div></div>
-      )
-    }
+export default function App() {
+  const [height, setHeight] = useState("");
+  const [weight, setWeight] = useState("");
 
-    export default App
+  const [bmiResult, setBmiResult] = useState(null);
+
+  const [status, setStatus] = useState("");
+
+  function calculateBMI() {
+    let bmi = Number(weight / (height / 100) ** 2).toFixed(2);
+    setBmiResult(bmi);
+
+    let bmiStatus = getStatus(bmi);
+
+    setStatus(bmiStatus);
+
+    setHeight("");
+    setWeight("");
+  }
+
+  function getStatus(bmi) {
+    if (bmi < 18.5) return "Underweight";
+    else if (bmi >= 18.5 && bmi < 24.9) return "Normal";
+    else if (bmi >= 25 && bmi < 29.9) return "Overweight";
+    else return "Obese";
+  }
+
+  return (
+    <div className="w-full max-w-xs m-10">
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <h1 className="text-center mb-4 text-xl"> BMI Calculator</h1>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            for="username"
+          >
+            Height
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="Height "
+            type="text"
+            placeholder="Height in cm"
+            value={height}
+            onChange={(e) => setHeight(e.target.value)}
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            for="password"
+          >
+            Weight
+          </label>
+          <input
+            className="shadow appearance-none  rounded w-full py-2 "
+            id="Weight"
+            type="Weight in kg"
+            placeholder="Weight in cm"
+            value={weight}
+            onChange={(e) => setWeight(e.target.value)}
+          />
+        </div>
+        <div className="flex items-center justify-center">
+          <button
+            className="bg-purple-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+            onClick={calculateBMI}
+          >
+            Calculate BMI
+          </button>
+        </div>
+        {bmiResult && (
+          <div className="mt-4">
+            <p>Your BMI is: {bmiResult} </p>
+            <p>You are currently: {status}</p>
+          </div>
+        )}
+      </form>
+    </div>
+  );}
