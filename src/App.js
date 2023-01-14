8d7c7fd9571817586f3a82d47e411a153b6fb2b9
Prasham1710 <jainprasham17@gmail.com>
2023-01-15 00:47:23 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ export default function App() {
         <div className="mb-4">
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
-            for="username"
+            
           >
             Height
           </label>
@@ -52,7 +52,7 @@ export default function App() {
         <div className="mb-6">
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
-            for="password"
+            
           >
             Weight
           </label>
